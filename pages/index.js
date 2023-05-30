0edc79887ae49dbfe0c000f2890360333d9b90aa
Salohiddin <71985333+iCoderBoy@users.noreply.github.com>
2023-05-30 16:18:07 +0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Hero from "@/components/UI/Hero";
+import Projects from "@/components/UI/Projects/Projects";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero/>
+  
+    
+ 
+      <Projects/>
+
+    </>  
+    )
+}
