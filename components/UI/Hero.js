4e81754b09c099c13ffc497e182153000f7b592e
Salohiddin <71985333+iCoderBoy@users.noreply.github.com>
2023-06-09 22:52:39 +0500

    Hero.js

--- components/UI/Hero.js
+++ components/UI/Hero.js
@@ -22,7 +22,7 @@ export default function Hero() {
                     </span></h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptas dolore quos molestiae at sequi dicta quia hic reprehenderit sapiente quo debitis, asperiores delectus eaque!</p>
                     <a href='#'>Download CV</a>
-                    <a href='#'>ContactMe</a>
+                    <a href='#'>Contact Me</a>
                 </div>
                 <div className={styles.imageSide}>
                     <div className={styles.toogleOne}>
@@ -30,12 +30,12 @@ export default function Hero() {
                         <h3>FullStack</h3>
                     </div>
                     <div className={styles.toogleTwo}>
-                    <span><DesktopMacIcon fontSize='large'/></span>
-                        <h3>UI/UX </h3>
+                        <span><DesktopMacIcon fontSize='large'/></span>
+                        <h3>UI/UX</h3>
                     </div>
 
                     <div className={styles.imgBox}>
-                    <Image src="/Images/hero.png" width={300} height={400}/>
+                        <Image src="/Images/hero.png" width={300} height={400}/>
                     </div>
                 </div>
             </div>
